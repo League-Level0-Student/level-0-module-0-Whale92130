71c695243ad7af4003e11582999d7bb9eff8cd3a
student <student@ip-172-31-61-199.us-west-2.compute.internal>
2021-08-03 19:53:25 -0700

    Did awesome or not and Scray maze Started Dragon fight

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,25 +2,39 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
-public class AwesomeOrNot {
+import javax.swing.JOptionPane;
 
+public class AwesomeOrNot {
+	public static void main(String[] args) {
 	// 1. Make a main method that includes everything below
 
 	
 		Random ran = new Random();    //This will be used below to make a random number. 
 		
 		// 2. Make a variable that will hold a random whole number
-	
+		int number1 = 0;
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
-	
+		number1 = ran.nextInt(4);
 		// 3. Print your variable to the console
 	
 		// 4. Get the user to enter something that they think is awesome
-	
+		String Anser = JOptionPane.showInputDialog("What is something awesome");
 		// 5. If your variable is  0
-	
+		if (number1 == (0)) {
+			System.out.println("Thats Awesome");
+		}
 			// -- tell the user whatever they entered is awesome!
-	
+		if (number1 == (1)) {
+			System.out.println("Thats Ok");
+		}
+		
+		if (number1 == (2)) {
+			System.out.println("Thats Boring");
+		}
+		
+		if (number1 == (3)) {
+			System.out.println("Nice");
+		}
 		// 6. If your variable is  1
 	
 			// -- tell the user whatever they entered is ok.
@@ -32,5 +46,5 @@ public class AwesomeOrNot {
 		// 8. If your variable is  3
 	
 			// -- invent your own message to give to the user (be nice).
-
+	}
 }
--- src/_04_amazing_games/_2_dragon_fight/DragonFight.java
+++ src/_04_amazing_games/_2_dragon_fight/DragonFight.java
@@ -14,13 +14,15 @@ public class DragonFight {
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 
 		// 1. Create some variables to hold health levels
-		
+		int Php = 100;
+		int Dhp = 100;
 			// playerHealth to store your health - set it equal to 100
-	
+		
 			// dragonHealth to store the dragon's health - set it equal to 100
 
 		// 2. Create some variables to hold the attack strengths. These will be given different values later. 
-		
+		int Patck = 0;
+		int Datck = 0;
 			// playerAttack to store the damage the player's attack will do - set it equal
 			// to 0 for now.
 	
@@ -32,29 +34,35 @@ public class DragonFight {
 		while (true) {
 
 			// THE PLAYER ATTACKS THE DRAGON
-
+			System.out.println("A dragon has attacked you");
+			String Q1 = JOptionPane.showInputDialog("Do you want to yell or kick");
 				// 3. Ask the player in a pop-up if they want to attack the dragon with a yell
 				// or a kick
 	
 				// 4. If they typed in "yell":
-	
+			if (Q1 == ("yell")) {
+				Patck = ran.nextInt(10);
+			}
+			if (Q1 == ("kick")) {
+				Patck = ran.nextInt(25);
+			}
 					  // -- Find a random number between 0 and 10 and store it in playerAttack. Use
 					  // ran.nextInt(10)
 			
 				// 5. If they typed in "kick":
 	
 					  // -- Find a random number between 0 and 25 and store it in playerAttack.
-		
+			System.out.println("The Dragon RETALIATES");
 				// 6. Subtract the player attack value from the dragon's health
-
+			Dhp = Dhp - Patck;
 			// THE DRAGON RETALIATES
 
 				// 7. Find a random number between 0 and 35 and store it in dragonAttack
-	
+			Datck = ran.nextInt(35);
 				// 8. Subtract the dragon attack value from the player's health
-
+			Php = Php - Datck;
 			// ASSESS THE DAMAGE
-
+			
 				// 9. If the player's health is less than or equal to 0, the game is over,
 				//    call the playerLost() method
 	
